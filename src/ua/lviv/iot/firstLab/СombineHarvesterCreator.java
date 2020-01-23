ee614bf59408cebd03bca519af47372828ba38e8
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-01-23 10:48:14 +0200

    Update and rename KombainJob.java to СombineHarvesterCreator.java

---
+++ src/ua/lviv/iot/firstLab/СombineHarvesterCreator.java
@@ -0,0 +1,34 @@
+package ua.lviv.iot.firstLab;
+
+public class СombineHarvesterCreator {
+
+    public static void main(String[] args){
+
+        СombineHarvester firstсombain = new СombineHarvester();
+        СombineHarvester secondсombain = new СombineHarvester(52.5, 475.9, 3, "green");
+        СombineHarvester thirdсombain = new СombineHarvester(54.4, 32.7, 4, "black", 4, "Petro", 1);
+        firstсombain.toString();
+        System.out.println("*****FirstСombain********");
+        System.out.println(firstсombain);
+        System.out.println("*****SecondKombain********");
+        secondсombain.toString();
+        System.out.println(secondсombain);
+        System.out.println("*****ThirdKombain********");
+        thirdсombain.toString();
+        System.out.println(thirdсombain);
+        System.out.println("***Zuklu****");
+
+        СombineHarvester[] arrayOfСombain = new СombineHarvester[4];
+        int somenum = 0;
+        do {
+            arrayOfСombain[somenum] = new СombineHarvester();
+            somenum++;
+        }
+        while (somenum < 4);
+        for(СombineHarvester a:arrayOfСombain){
+            a.toString();
+            System.out.println(a);
+        }
+
+    }
+}
